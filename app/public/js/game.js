0cd845aaa481edd80338c7d307c1036910df84d2
shamseen <shamseen.c@gmail.com>
2021-04-03 17:42:13 -0400

    game setup - renders blank game

---
+++ app/public/js/game.js
@@ -0,0 +1,18 @@
+/* ---- Setup ---- */
+const config = {
+    /* what it will use to render
+       if WebGL fails, uses Canvas */
+    type: Phaser.AUTO,
+
+    // size of canvas element
+    width: 800,
+    height: 600,
+};
+
+const game = new Phaser.Game(config);
+
+function preload() { }
+
+function create() { }
+
+function update() { }
